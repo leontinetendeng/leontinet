b84c050b754b117a1427c69f0a818ca629bdd32f
leontinetendeng <leontinetendeng46@gmail.com>
2026-02-28 15:41:15 +0000

    Update CV.py

--- CV.py
+++ CV.py
@@ -6,12 +6,12 @@ st.set_page_config(page_title="CV - Léontine Tendeng", layout="centered")
 # Titre
 st.header("Léontine Théodoria Élisabeth TENDENG")
 
-# Informations personnelles
+# Information personnelle
 st.sidebar.title ("Léontine Théodoria Elisabeth TENDENG")
-st.write(" Information personnelle")
-st.write(" Adresse : Dakar, Sénégal")
-st.write(" Email : leontinetendeng46@email.com")
-st.write(" Nationalité : Sénégalaise")
+st.sidebar.write(" Information personnelle")
+st.sidebar.write(" Adresse : Dakar, Sénégal")
+st.sidebar.write(" Email : leontinetendeng46@email.com")
+st.sidebar.write(" Nationalité : Sénégalaise")
 # Profil
 st.subheader(" Profil")
 st.write("""Professionnelle en Geomatique je suis capable d'intervenir sur le terrain
